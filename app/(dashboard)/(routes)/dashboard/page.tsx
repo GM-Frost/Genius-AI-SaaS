16365e6d0a5a152e2920d7fdcc84ecde3b29c5e2
Frosty <nayanbastola777@gmail.com>
2023-11-14 19:15:39 -0500

    Add Mobile Sidebar

--- app/(dashboard)/(routes)/dashboard/page.tsx
+++ app/(dashboard)/(routes)/dashboard/page.tsx
@@ -1,5 +1,7 @@
-import { Button } from "@/components/ui/button";
-
 export default function DashboardPage() {
-  return <p>Dashboard page (Protected)</p>;
+  return (
+    <div>
+      <p>Dashboard page (Protected)</p>
+    </div>
+  );
 }
--- app/layout.tsx
+++ app/layout.tsx
@@ -5,7 +5,7 @@ import { ClerkProvider } from "@clerk/nextjs";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: "Brains-AI",
+  title: "Genius-AI",
   description: "AI Platform",
 };
 
